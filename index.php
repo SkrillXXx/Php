41c5420c40cce4bbc096cbdeaee9c08c7c4ac9a3
Студент <student@ad.anosov.ru>
2024-09-23 09:28:39 +0500

    first

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+         <title>
+           Kast_Bulat_anime_go
+         </title>
+    </head>
+    <body>
+    <?php
+    
+    ?>
+    
+    </body> 
+</html>
