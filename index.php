600d4fc8f130eec463e461d49917295f7bb18bbb
Студент <student@ad.anosov.ru>
2024-09-23 10:39:32 +0500

    two

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
+<?php
+  include_once "gen.php"
+?>
 <!DOCTYPE HTML>
 <html>
     <head>
+      <link href="style.css" rel="stylesheet">
          <title>
            Kast_Bulat_anime_go
          </title>
     </head>
     <body>
     <?php
-    
+    gen_table();
     ?>
-    
     </body> 
 </html>
